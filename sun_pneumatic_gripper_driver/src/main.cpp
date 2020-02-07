929ffea5d3b5bb4df3ed11d43d75b4c27a2f6a99
Gaetano Colella <gaetano.colella@studenti.unicampania.it>
2020-02-07 17:31:48 +0100

    write char on serial channel

--- sun_pneumatic_gripper_driver/src/main.cpp
+++ sun_pneumatic_gripper_driver/src/main.cpp
@@ -10,29 +10,41 @@
 #define CRESET          "\033[0m"
 
 using namespace std;
-
+#define ON  '1'
+#define OFF '0'
 
 //------------------------------------------------------------------------
 // Global variables
 //------------------------------------------------------------------------
+uint8_t b2write[1];
 
+
+// Connect to device using serial port
+
+/***** CHECK PARAMS ****/
+string serial_port = string("/dev/ttyACM0");
+unsigned long baud = 9600;
+int serialTimeout = 1000;
+
+/*** INIT SERIAL ****/	
+serial::Serial my_serial(serial_port, baud, serial::Timeout::simpleTimeout(serialTimeout));
 
 //------------------------------------------------------------------------
 // Function implementation
 //------------------------------------------------------------------------
-
-
-
 bool activate_suckerSrv(sun_pneumatic_gripper_common::ActivateSucker::Request &req, sun_pneumatic_gripper_common::ActivateSucker::Response &res){
 
 if(req.sucker1 && (req.sucker2_3 || !req.sucker2_3)){
 //attiva sucker1
+b2write[0] = ON;
 ROS_INFO("SUCKER 1 ON");
 } 
 else {
 //disattiva sucker1
+b2write[0] = OFF;
 ROS_INFO("SUCKER 1 OFF");
 }
+my_serial.write(b2write,1);
 
 if ((req.sucker1 || !req.sucker1) && req.sucker2_3){
 //attiva sucker2 e sucker3
@@ -48,37 +60,20 @@ return true;
 }
 
 
-/**
- * The main function
- */
-
 int main( int argc, char **argv )
 {
 
 ros::init(argc, argv, "pneumatic_gripper");
 ros::NodeHandle nh;
 
-// Connect to device using serial port
-
-/***** CHECK PARAMS ****/
-/*string serial_port = string("");
-nh.param("serial_port" , serial_port, string("/dev/ttyUSB0") );
-string str_baud = string("");
-nh.param("baud_rate" , str_baud, string("500000") );
-sscanf(str_baud.c_str(), "%lu", &baud);
-int serialTimeout = 0;
-nh.param("serial_timeout" , serialTimeout, 1000 );*/
 
 
-/*** INIT SERIAL ****/	
-//serial::Serial my_serial(serial_port, baud, serial::Timeout::simpleTimeout(serialTimeout));
-
-/*** CHECK ***/
-/*if(!my_serial.isOpen()){
+/*** CHECK SERIAL***/
+if(!my_serial.isOpen()){
    cout << ERROR_COLOR << "ERROR - SERIAL PORT " << WARN_COLOR << serial_port << ERROR_COLOR << " is not open!" << CRESET <<endl;
    exit(-1);
 }
-cout << SUCCESS_COLOR << "SERIAL PORT " << WARN_COLOR << serial_port << SUCCESS_COLOR << " OPEN - OK" << CRESET << endl;*/
+cout << SUCCESS_COLOR << "SERIAL PORT " << WARN_COLOR << serial_port << SUCCESS_COLOR << " OPEN - OK" << CRESET << endl;
 
 
 // Services
@@ -87,11 +82,6 @@ ros::ServiceServer activate_suckerSS;
 activate_suckerSS=nh.advertiseService("activate_sucker",activate_suckerSrv);
 
 //**** ROS MAIN LOOP  ***//
-while(ros::ok()){
-  
-//my_serial.write(b2write,1);
-
-ros::spinOnce();
-}
+ros::spin();
 
 }
